77981a2d26af9e5510b9e891e29843cbc19db8b0
Cristian <gonzalezgarciacristian@hotmail.com>
2020-03-11 16:14:18 +0100

    Added interface to tab3: configuration

--- src/uniovi/cgg/ui/MainApp.java
+++ src/uniovi/cgg/ui/MainApp.java
@@ -17,6 +17,7 @@ import javafx.stage.Stage;
 import uniovi.cgg.main.Main;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -26,19 +27,22 @@ public class MainApp extends Application {
 
 	private final int STAGE_WIDTH = 640;
 	private final int STAGE_HEIGHT = 480;
-	
+
 	private static final String i18n_PATH = "i18n/texts";
-	
+
 	private static ResourceBundle resourceBundle = null;
 
 	/**
-	 * Creamos y configuramos las pestañas 
-	 * @return TabPane Retorna todas las pestañas creadas y configuradas dentro del TabPane
+	 * Creamos y configuramos las pestañas
+	 * 
+	 * @return TabPane Retorna todas las pestañas creadas y configuradas dentro del
+	 *         TabPane
 	 */
 	private TabPane createAndConfigureTabs() {
 		TabPane tabPane = new TabPane();
 
-		Tab tab2 = new Tab(resourceBundle.getString("tab.two.title"), new Label(resourceBundle.getString("tab.two.label")));
+		Tab tab2 = new Tab(resourceBundle.getString("tab.two.title"),
+				new Label(resourceBundle.getString("tab.two.label")));
 
 		tabPane.getTabs().add(tabGenerator());
 		tabPane.getTabs().add(tab2);
@@ -47,82 +51,130 @@ public class MainApp extends Application {
 
 		return tabPane;
 	}
-	
+
 	/**
 	 * Creación y configuración de la pestaña 1: generador de textos
+	 * 
 	 * @return Tab pestaña ya creada configurada
 	 */
 	private Tab tabGenerator() {
 		Tab tab = new Tab(resourceBundle.getString("tab.one.title"));
-		
-		//Interface
+
+		// Interface
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
 		grid.setPadding(new Insets(25, 25, 25, 25));
-		
+
 		tab.setContent(grid);
-		
-		Button btnGenerate= new Button(resourceBundle.getString("tab.one.btnGenerate"));		
-        grid.add(btnGenerate, 0, 0);
-		
-        TextArea txtAGeneratedText = new TextArea(resourceBundle.getString("tab.one.txtAGeneratedText"));
+
+		Button btnGenerate = new Button(resourceBundle.getString("tab.one.btnGenerate"));
+		grid.add(btnGenerate, 0, 0);
+
+		TextArea txtAGeneratedText = new TextArea(resourceBundle.getString("tab.one.txtAGeneratedText"));
 		txtAGeneratedText.setPrefWidth(220);
 		grid.add(txtAGeneratedText, 0, 1);
-		
+
 		Label sendTo = new Label(resourceBundle.getString("tab.one.sendTo"));
 		grid.add(sendTo, 0, 2);
-		
-		TextField txtFSendTo = new TextField();		
+
+		TextField txtFSendTo = new TextField();
 		grid.add(txtFSendTo, 1, 2);
-		
+
 		CheckBox ccToMe = new CheckBox(resourceBundle.getString("tab.one.ccToMe"));
 		grid.add(ccToMe, 0, 3);
-		
+
 		// Actions
 		btnGenerate.setOnAction(e -> txtAGeneratedText.setText(new Main().generateText()));
-		
+
 		return tab;
 	}
 
 	/**
 	 * Creación y configuración de la pestaña 3: configuración
+	 * 
 	 * @return Tab pestaña ya creada configurada
 	 */
 	private Tab tabConfiguration() {
 		Tab tab = new Tab(resourceBundle.getString("tab.three.title"));
-		
-		Label currentLanguage = new Label(resourceBundle.getString("tab.three.currentLanguage")+Locale.getDefault().toString());
-		tab.setContent(currentLanguage);
+
+		// Interface
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+
+		tab.setContent(grid);
+
+		Label currentLanguage = new Label(
+				resourceBundle.getString("tab.three.currentLanguage") + Locale.getDefault().toString());
+		grid.add(currentLanguage, 0, 0);
+
+		ComboBox<String> comboBox = new ComboBox<String>();
+		comboBox.getItems().add(resourceBundle.getString("languageSpanish"));
+		comboBox.getItems().add(resourceBundle.getString("languageEnglish"));
+		grid.add(comboBox, 1, 0);
+
+		Label labelUserEmail = new Label(resourceBundle.getString("tab.three.userEmail"));
+		grid.add(labelUserEmail, 0, 1);
+
+		TextField tftFUserEmail = new TextField();
+		grid.add(tftFUserEmail, 1, 1);
+
+		Label labelEmailHead = new Label(resourceBundle.getString("tab.three.emailIntroduction"));
+		grid.add(labelEmailHead, 0, 2);
+
+		TextArea txtAEmailIntroduction = new TextArea();
+		txtAEmailIntroduction.setPrefWidth(220);
+		grid.add(txtAEmailIntroduction, 1, 2);
+
+		Label labelEmailSign = new Label(resourceBundle.getString("tab.three.emailEnd"));
+		grid.add(labelEmailSign, 0, 3);
+
+		TextArea txtAEmailSign = new TextArea();
+		txtAEmailSign.setPrefWidth(220);
+		grid.add(txtAEmailSign, 1, 3);
+
+		Button btnSave = new Button(resourceBundle.getString("tab.three.btnSave"));
+		grid.add(btnSave, 0, 4);
+
+		Button btnWithoutSave = new Button(resourceBundle.getString("tab.three.btnWithoutSave"));
+		grid.add(btnWithoutSave, 1, 4);
 
 		return tab;
 	}
-	
+
 	/**
 	 * Creación y configuración de la pestaña 4: Acerca de...
+	 * 
 	 * @return Tab pestaña ya creada configurada
 	 */
 	private Tab tabAbout() {
-		Tab tab = new Tab(resourceBundle.getString("tab.four.title"), new Label(resourceBundle.getString("tab.four.label")));		
+		Tab tab = new Tab(resourceBundle.getString("tab.four.title"),
+				new Label(resourceBundle.getString("tab.four.label")));
 
 		return tab;
 	}
 
 	/**
 	 * Saca la resolución de la pantalla y la retorna
-	 * @return Double Retorna un array de dos posiciones de tipo double con el ancho y alto
+	 * 
+	 * @return Double Retorna un array de dos posiciones de tipo double con el ancho
+	 *         y alto
 	 */
 	private double[] getScreenResolution() {
 		Rectangle2D screenProperties = Screen.getPrimary().getBounds();
 
-		double[] screenResolution = {screenProperties.getWidth(), screenProperties.getHeight()};
+		double[] screenResolution = { screenProperties.getWidth(), screenProperties.getHeight() };
 
 		return screenResolution;
 	}
 
 	/**
 	 * Establece que el programa se abra justo en el medio de la pantalla
+	 * 
 	 * @param stage
 	 * @return stage
 	 */
@@ -142,18 +194,19 @@ public class MainApp extends Application {
 	}
 
 	/**
-	 * Carga la ruta dónde estan los textos en diferentes idiomas. La ruta tiene que estar dentro del SRC. Por defecto carga el idioma local
-	 * Idiomas disponibles: es, en
+	 * Carga la ruta dónde estan los textos en diferentes idiomas. La ruta tiene que
+	 * estar dentro del SRC. Por defecto carga el idioma local Idiomas disponibles:
+	 * es, en
 	 */
 	private void loadi18n() {
-		//Locale.setDefault(new Locale("en")); // es españa, en inglés
+		// Locale.setDefault(new Locale("en")); // es españa, en inglés
 		resourceBundle = ResourceBundle.getBundle(i18n_PATH);
 	}
 
 	@Override
 	public void start(Stage stage) {
 		loadi18n();
-		
+
 		TabPane tabPane = createAndConfigureTabs();
 
 		VBox vBox = new VBox(tabPane);
