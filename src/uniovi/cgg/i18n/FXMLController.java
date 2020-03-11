abafd95baeda9595d63c56b9a16ae56d84e0549a
Cristian <gonzalezgarciacristian@hotmail.com>
2020-03-11 11:48:45 +0100

    Added i18n

---
+++ src/uniovi/cgg/i18n/FXMLController.java
@@ -0,0 +1,15 @@
+package uniovi.cgg.i18n;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FXMLController implements Initializable {
+	
+	@Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+       
+    }
+
+}
--- src/uniovi/cgg/ui/MainApp.java
+++ src/uniovi/cgg/ui/MainApp.java
@@ -1,85 +1,129 @@
 package uniovi.cgg.ui;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 
 public class MainApp extends Application {
-	
+
 	private final int STAGE_WIDTH = 640;
 	private final int STAGE_HEIGHT = 480;
 
+	private static final String SPANISH = "es";
+	private static final String ENGLISH = "en";
+	
+	private static final String i18n_PATH = "i18n/texts";
+	
+	private static ResourceBundle resourceBundle = null;
+
 	/**
-	 * Creamos y configuramos las pestañas
-	 * 
-	 * @return
+	 * Creamos y configuramos las pestañas 
+	 * @return TabPane Retorna todas las pestañas creadas y configuradas dentro del TabPane
 	 */
 	private TabPane createAndConfigureTabs() {
 		TabPane tabPane = new TabPane();
 
-		Tab tab1 = new Tab("Texto", new Label("Show all planes available"));
-		Tab tab2 = new Tab("Otro", new Label("Show all cars available"));
-		Tab tab3 = new Tab("Configuración", new Label("Show all boats available"));
-		
+		Tab tab1 = new Tab(resourceBundle.getString("tab.one.title"), new Label(resourceBundle.getString("tab.one.label")));
+		Tab tab2 = new Tab(resourceBundle.getString("tab.two.title"), new Label(resourceBundle.getString("tab.two.label")));
 
 		tabPane.getTabs().add(tab1);
 		tabPane.getTabs().add(tab2);
-		tabPane.getTabs().add(tab3);
+		tabPane.getTabs().add(tabConfiguration());
 		tabPane.getTabs().add(tabAbout());
 
 		return tabPane;
 	}
-	
-	private Tab tabAbout() {		
-		Tab tab4 = new Tab("Acerca de...", new Label("Cristian González García, UniOvi, 2020"));
+
+	/**
+	 * Creación y configuración de la pestaña 3: configuración
+	 * @return Tab pestaña ya creada configurada
+	 */
+	private Tab tabConfiguration() {
+		Tab tab = new Tab(resourceBundle.getString("tab.three.title"));
 		
-		return tab4;
+		Label currentLanguage = new Label(resourceBundle.getString("tab.three.currentLanguage")+Locale.getDefault().toString());
+		tab.setContent(currentLanguage);
+
+		return tab;
 	}
-	
+	/**
+	 * Creación y configuración de la pestaña 4: Acerca de...
+	 * @return Tab pestaña ya creada configurada
+	 */
+	private Tab tabAbout() {
+		Tab tab = new Tab(resourceBundle.getString("tab.four.title"), new Label(resourceBundle.getString("tab.four.label")));		
+
+		return tab;
+	}
+
+	/**
+	 * Saca la resolución de la pantalla y la retorna
+	 * @return Double Retorna un array de dos posiciones de tipo double con el ancho y alto
+	 */
 	private double[] getScreenResolution() {
 		Rectangle2D screenProperties = Screen.getPrimary().getBounds();
-		
+
 		double[] screenResolution = {screenProperties.getWidth(), screenProperties.getHeight()};
-		
-		return screenResolution;		
+
+		return screenResolution;
 	}
-	
-	private Stage stageProperties(Stage stage) {				
-		stage.setTitle("Companies Legal Information Generator");		
-		
+
+	/**
+	 * Establece que el programa se abra justo en el medio de la pantalla
+	 * @param stage
+	 * @return stage
+	 */
+	private Stage stageProperties(Stage stage) {
+		stage.setTitle(resourceBundle.getString("main.title"));
+
 		double[] screenResolution = getScreenResolution();
-		
+
 		// Stage in the middle
-		stage.setX((screenResolution[0]-STAGE_WIDTH)*0.5);
-		stage.setY((screenResolution[1]-STAGE_HEIGHT)*0.5);		
-		
+		stage.setX((screenResolution[0] - STAGE_WIDTH) * 0.5);
+		stage.setY((screenResolution[1] - STAGE_HEIGHT) * 0.5);
+
 		stage.setWidth(STAGE_WIDTH);
 		stage.setHeight(STAGE_HEIGHT);
-		
-		return stage;		
+
+		return stage;
+	}
+
+	/**
+	 * Carga la ruta dónde estan los textos en diferentes idiomas. La ruta tiene que estar dentro del SRC. Por defecto carga el idioma local
+	 * Idiomas disponibles: es, en
+	 */
+	private void loadi18n() {
+		//Locale.setDefault(new Locale("es")); // es españa, en inglés
+		resourceBundle = ResourceBundle.getBundle(i18n_PATH);
 	}
 
 	@Override
 	public void start(Stage stage) {
+		loadi18n();
+		
 		TabPane tabPane = createAndConfigureTabs();
 
 		VBox vBox = new VBox(tabPane);
-		
+
+		// Scene contiene lo que se quiere mostrar dentro del Stage
 		Scene scene = new Scene(vBox);
 		stage.setScene(scene);
-		
+
 		stage = stageProperties(stage);
-		
+
 		stage.show();
 	}
 
