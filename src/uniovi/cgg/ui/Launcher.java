8ae121ffe031a4ab0f22f2790fbed8199ff63b9a
Cristian González García <gonzalezgarciacristian@hotmail.com>
2020-04-16 19:27:49 +0200

    Created a class to create a runnable jar and avoiding the problem of JavaFX

---
+++ src/uniovi/cgg/ui/Launcher.java
@@ -0,0 +1,15 @@
+package uniovi.cgg.ui;
+
+/**
+ * This class is needed to create the runnable Jar and avoiding the JavaFX error: Error: JavaFX runtime components are missing, and are required to run this
+application. The, to create the runnable Jar is required to select this class as main class.
+ * @author Cristian González García
+ *
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+		MainApp.main(args);
+	}
+
+}
